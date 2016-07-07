2e9aaafb0630373b1e77b068190956a2a582ac2c
Shoeb Mohammed <shoeb.ahmed@hotmail.com>
2016-07-07 17:48:24 -0500

    master: completed until ex:3.23

--- my_solutions_for_exercises/ch3/3_4.scm
+++ my_solutions_for_exercises/ch3/3_4.scm
@@ -23,6 +23,12 @@
     (expression
       ("let" (arbno  identifier "=" expression) "in" expression)
       let-exp)
+    (expression
+     ("proc" "(" (separated-list identifier ",") ")" expression)
+     proc-exp)
+    (expression
+     ("(" expression (arbno expression) ")")
+     app-exp)
     (primitive ("+")     add-prim)
     (primitive ("-")     subtract-prim)
     (primitive ("*")     mult-prim)
@@ -36,6 +42,7 @@
 
 (define show-the-datatypes
   (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
 (define scan&parse
   (sllgen:make-string-parser the-lexical-spec the-grammar))
 
@@ -74,10 +81,17 @@
       (let-exp (ids rands body)  ;\new3
         (let ((args (eval-rands rands env)))
           (eval-expression body (extend-env ids args env))))
+      (proc-exp (ids body) (closure ids body env))
+      (app-exp (rator rands)
+	       (let ((proc (eval-expression rator env))
+		     (args (eval-rands rands env)))
+		 (if (procval? proc)
+		     (apply-procval proc args)
+		     (eopl:error 'eval-expression
+				 "Attempt to apply non-procedure ~s" proc))))
       (else (eopl:error 'eval-expression "Not here:~s" exp))
       )))
 
-;;;; Right now a prefix must appear earlier in the file.
 
 (define eval-rands
   (lambda (rands env)
@@ -95,7 +109,6 @@
       (mult-prim  () (* (car args) (cadr args)))
       (incr-prim  () (+ (car args) 1))
       (decr-prim  () (- (car args) 1))
-;&      
       (zero-test-prim () (if (zero? (car args)) 1 0))
       )))
 
@@ -105,6 +118,22 @@
       '(i v x)
       '(1 5 10)
       (empty-env))))
+
+
+;;;;;;;;;;;;;;;; closure ;;;;;;;;;;;;;;;;
+
+(define-datatype procval procval?
+  (closure
+   (ids (list-of symbol?))
+   (body expression?)
+   (env environment?)))
+
+(define apply-procval
+  (lambda (proc args)
+    (cases procval proc
+	   (closure (ids body env)
+		    (eval-expression body (extend-env (ids args env)))))))
+
 
 ;;;;;;;;;;;;;;;; booleans ;;;;;;;;;;;;;;;;
 
@@ -161,85 +190,16 @@
                 #f))))))
 
 
-
-;;;;END FROM BOOK;;;;;
-
-
-
-
-
-
-;;3.16
-					;run above code and test let expressions
-
-
-
-;;3.17
-(define apply-primitive
-  (lambda (prim args)
-    (cases primitive prim
-      (add-prim  () (+ (car args) (cadr args)))
-      (subtract-prim () (- (car args) (cadr args)))
-      (mult-prim  () (* (car args) (cadr args)))
-      (incr-prim  () (+ (car args) 1))
-      (decr-prim  () (- (car args) 1))
-      (zero-test-prim () (if (zero? (car args)) 1 0))
-      (list-prim () args)
-      (cons-prim () (cons (car args) (cadr args)))
-      (car-prim () (caar args))
-      (cdr-prim () (cdar args))
-      (eq?-prim () (eq? (car args) (cadr args)))
-      )))
-
-					;We couldn't adquately test this until now because never had a way to bind variable occurences.
-					;That is, we could only test things like (eq? 1 1) and not (eq? name1 name2)
-
-
-
-;;3.18
-
-(define the-lexical-spec
-  '((whitespace (whitespace) skip)
-    (comment ("%" (arbno (not #\newline))) skip)
-    (identifier
-      (letter (arbno (or letter digit "_" "-" "?")))
-      symbol)
-    (number (digit (arbno digit)) number)))
-
-(define the-grammar-b
-  '((program (expression) a-program)
-    (expression (number) lit-exp)
-    (expression (identifier) var-exp)   
-    (expression
-      (primitive "(" (separated-list expression ",") ")")
-      primapp-exp)
-    (expression
-      ("if" expression "then" expression "else" expression)
-      if-exp)
-    (expression
-      ("let" (arbno  identifier "=" expression) "in" expression)
-      let-exp)
-    (epxression
-     ("unpack" (arbno identifier) "=" expression "in" expression)
-     unpack-exp)
-    (primitive ("+")     add-prim)
-    (primitive ("-")     subtract-prim)
-    (primitive ("*")     mult-prim)
-    (primitive ("add1")  incr-prim)
-    (primitive ("sub1")  decr-prim)
-    (primitive ("zero?") zero-test-prim)
-    
-    ))
-
-(sllgen:make-define-datatypes the-lexical-spec the-grammar-b)
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
 
 (define show-the-datatypes
-  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar-b)))
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
 (define scan&parse
-  (sllgen:make-string-parser the-lexical-spec the-grammar-b))
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
 
 (define just-scan
-  (sllgen:make-string-scanner the-lexical-spec the-grammar-b))
+  (sllgen:make-string-scanner the-lexical-spec the-grammar))
 
 (define read-eval-print 
   (sllgen:make-rep-loop  "--> "
@@ -247,8 +207,15 @@
                                         ; dependency 
     (sllgen:make-stream-parser 
       the-lexical-spec
-      the-grammar-b)))
+      the-grammar)))
 
+
+
+;;3.19
+					;just run the test from the book using interpreter above
+
+
+;;3.20
 
 (define eval-expression 
   (lambda (exp env)
@@ -265,13 +232,56 @@
       (let-exp (ids rands body)  ;\new3
         (let ((args (eval-rands rands env)))
           (eval-expression body (extend-env ids args env))))
-      (unpack-exp (ids exp body)
-		  (let ((args (eval-expression exp)))
-		    (if (= (length ids) (length args))
-			(eval-expression body (extend-env ids args env))
-			(eopl:error 'unpack-exp
-				    "Number of ids ~s do not match argument expression ~s"
-				    ids exp))))
+      (proc-exp (ids body) (closure ids body env))
+      (app-exp (rator rands)
+	       (let ((proc (eval-expression rator env)))
+		 (if (procval? proc)		 
+		     (cases procval proc
+			    (closure (proc-ids proc-body proc-env)
+				     (if (not (= (length proc-ids) (length rands)))
+					 (eopl:error 'app-exp
+						     "Number of formal arguments ~s don't match the number of actual arguments ~s for procedure application ~s"
+						     (length proc-ids) (length rands) proc)
+					 (let ((args (eval-rands rands env)))
+					   (apply-procval proc args)))))
+		     (eopl:error 'eval-expression
+				 "Attempt to apply non-procedure ~s" proc))))
       (else (eopl:error 'eval-expression "Not here:~s" exp))
       )))
 
+
+
+
+;;3.21
+let fac-maker = proc (maker , n)
+if n
+then *(n, (maker maker -(n,1)))
+else 1
+
+in proc (n)
+(fac-maker fac-maker n)
+
+
+
+;;3.22
+let even-maker = proc(maker , n)
+if n
+then (maker maker -(n,1))
+else
+1
+in
+let odd-maker = proc(maker, n)
+if -(n,1)
+then (maker even-maker -(n,1))
+else
+1
+in proc(n)
+(even-maker odd-maker n)
+
+;;similarly for odd
+
+
+
+;;3.23
+
+ 
